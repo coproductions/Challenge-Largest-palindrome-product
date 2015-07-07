fea4de552dac68dae34976e7618d7c5b00f496dd
Constanti <constantin.dumba@gmail.com>
2015-07-06 14:58:42 -1000

    combined it into one function that can take any digit value

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -12,102 +12,31 @@ module.exports = function(digits){
   var palindromeNumber = 0;
   var palindromeString;
 
-  if(digits === 2){
-    twoDigitPalindromeFactor();
-  } else if ( digits === 3){
-    palindromeThreeDigAlt();
-  } else if (digits === 4){
-    palindromeFourDigAlt();
-  } else if (digits === 5){
-    palindromeFiveDigAlt();
-  }
+  calculateHigestFactors(digits)
 
-  function twoDigitPalindromeFactor(){
-      for (var i = 99; i >= 10; i--) {
-        for (var j = 99; i >= 10; j--) {
-          palindromeNumber = i*j;
-          palindromeString = String(palindromeNumber);
+  function calculateHigestFactors(number){
+    var array = [];
+    var n = number;
+    while(n>0){
+      array.push(9);
+      n--
+    }
+    var startNum = Number(array.join(''));
+    var minFactor =Math.pow(10,digits)/10;
+    for (var i=startNum ; i>0; i--){
 
-          if(palindromeString.charAt(0) === palindromeString.charAt(palindromeString.length-1)){
-            if(palindromeString.charAt(1) === palindromeString.charAt(palindromeString.length-2)){
-              factor_0 = i;
-              factor_1 = j;
-              return;
-
-            }
-          }
-        };
-      };
-  }
-
-  function palindromeThreeDigAlt(){
-    var palindromeArray = [];
-    for (var outest = 9; outest >= 0; outest--) {
-      for (var mid =  9; mid >= 0; mid--) {
-        for (var inner = 8; inner >= 0; inner--) {
-          var palindromeArray = [];
-          palindromeArray.push(outest,mid,inner,inner,mid,outest)
-          palindromeNumber = Number(palindromeArray.join(''));
-          for (var fact = 999; fact >= 100; fact--) {
-             if(palindromeNumber%fact === 0 && palindromeNumber/fact<=999){
-                factor_0 = fact;
-                factor_1 = palindromeNumber/fact;
-                return;
-              }
-          };
-        };
+      var firstHalfArray = i.toString().split('');
+      palindromeNumber = Number(firstHalfArray.join('').concat(firstHalfArray.reverse().join('')))
+      for (var fact = startNum; fact >= minFactor; fact--) {
+        if(palindromeNumber%fact === 0 && palindromeNumber/fact<=startNum){
+          factor_0 = fact;
+          factor_1 = palindromeNumber/fact;
+          return;
+        }
       }
     }
   }
 
-  function palindromeFourDigAlt(){
-    var palindromeArray = [];
-    for (var outest = 9; outest >= 0; outest--) {
-      for (var mid =  9; mid >= 0; mid--) {
-        for (var inner = 9; inner >= 0; inner--) {
-          for (var innest = 8; innest >= 0; innest--) {
-
-            var palindromeArray = [];
-            palindromeArray.push(outest,mid,inner,innest,innest,inner,mid,outest)
-            palindromeNumber = Number(palindromeArray.join(''));
-            for (var fact = 9999; fact >= 1000; fact--) {
-               if(palindromeNumber%fact === 0 && palindromeNumber/fact<=9999){
-                  factor_0 = fact;
-                  factor_1 = palindromeNumber/fact;
-                  return;
-                }
-            };
-          }
-        };
-      }
-    }
-  }
-
-  function palindromeFiveDigAlt(){
-    var palindromeArray = [];
-    for (var outest = 9; outest >= 0; outest--) {
-      for (var mid =  9; mid >= 0; mid--) {
-        for (var inner = 9; inner >= 0; inner--) {
-          for (var innest = 9; innest >= 0; innest--) {
-            for (var innerest = 8; innerest >= 0; innerest--) {
-
-            var palindromeArray = [];
-            palindromeArray.push(outest,mid,inner,innest,innerest,innerest,innest,inner,mid,outest)
-            palindromeNumber = Number(palindromeArray.join(''));
-            for (var fact = 99999; fact >= 10000; fact--) {
-               if(palindromeNumber%fact === 0 && palindromeNumber/fact<=99999){
-                  factor_0 = fact;
-                  factor_1 = palindromeNumber/fact;
-                  return;
-                }
-
-              }
-            };
-          }
-        };
-      }
-    }
-  }
 
   return {
     factor_0 : factor_0,
